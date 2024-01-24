06b90057adca8932cc94717f7421ba83a03c860c
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2024-01-24 23:20:28 +0100

    add telescope treesitter keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -145,26 +145,29 @@ vim.api.nvim_set_keymap(
   { noremap = true, desc = '[o]pen file [b]rowser with the path of the [c]urrent buffer' }
 )
 
+local telescope = require 'telescope.builtin'
+
 -- Telescope git keybindings
-vim.keymap.set('n', '<leader>gt', require('telescope.builtin').git_status)
-vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_commits)
-vim.keymap.set('n', '<leader>gb', require('telescope.builtin').git_branches)
+vim.keymap.set('n', '<leader>gt', telescope.git_status)
+vim.keymap.set('n', '<leader>gc', telescope.git_commits)
+vim.keymap.set('n', '<leader>gb', telescope.git_branches)
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  telescope.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = true,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>th', require('telescope.builtin').colorscheme, { desc = '[th]emes' })
-vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
-vim.keymap.set('n', '<leader>fo', require('telescope.builtin').oldfiles, { desc = '[F]ind [O]ld files' })
+vim.keymap.set('n', '<leader>th', telescope.colorscheme, { desc = '[th]emes' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>gf', telescope.git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = '[F]ind [H]elp' })
+vim.keymap.set('n', '<leader>fw', telescope.grep_string, { desc = '[F]ind current [W]ord' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = '[F]ind by [G]rep' })
+vim.keymap.set('n', '<leader>fd', telescope.diagnostics, { desc = '[F]ind [D]iagnostics' })
+vim.keymap.set('n', '<leader>fo', telescope.oldfiles, { desc = '[F]ind [O]ld files' })
+vim.keymap.set('n', '<leader>ts', telescope.treesitter, { desc = 'Telescope [t]ree[s]itter view' })
