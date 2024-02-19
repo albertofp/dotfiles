91875d4bc120f9052bdbf6c5c8fd819a21ccc102
albertofp <albertopluecker@gmail.com>
2024-02-19 15:27:51 +0100

    use lsp fold provider, add peek keymap

--- .config/nvim/lua/custom/plugins/ufo.lua
+++ .config/nvim/lua/custom/plugins/ufo.lua
@@ -2,20 +2,25 @@ return {
   'kevinhwang91/nvim-ufo',
   dependencies = {
     'kevinhwang91/promise-async',
-    'nvim-treesitter/nvim-treesitter',
   },
 
   config = function()
     local ufo = require 'ufo'
     require('ufo').setup {
       provider_selector = function()
-        return { 'treesitter', 'indent' }
+        return { 'lsp', 'indent' }
       end,
 
       vim.keymap.set('n', 'zR', ufo.openAllFolds),
       vim.keymap.set('n', 'zM', ufo.closeAllFolds),
       vim.keymap.set('n', '-', '<cmd>foldclose<CR>', { silent = true, desc = 'Close fold' }),
       vim.keymap.set('n', '+', '<cmd>foldopen<CR>', { silent = true, desc = 'Open fold' }),
+      vim.keymap.set('n', 'zK', function()
+        local winid = ufo.peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, { silent = true, desc = 'Peek fold' }),
     }
   end,
 }
