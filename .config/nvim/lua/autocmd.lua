dcd9957195eac117ebe0b5b5ba07c93db54e1264
albertofp <albertopluecker@gmail.com>
2024-02-27 22:03:05 +0100

    nvim: restore last cursor position

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -6,3 +6,13 @@ vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
 vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
 vim.cmd([[let g:terraform_fmt_on_save=1]])
 vim.cmd([[let g:terraform_align=1]])
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Open file at the last position it was open at",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
