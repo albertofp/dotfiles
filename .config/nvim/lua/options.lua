68097c582a4e8530c7b957a2d15a3384ad82d5c4
Alberto Fontenelle Pluecker <alberto@cinference.bio>
2024-02-28 17:11:02 +0100

    nvim: remove fillchars

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -53,3 +53,6 @@ vim.opt.virtualedit = 'block'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- Eliminates the ~ after EOF
+vim.opt.fillchars = { eob = ' ' }
