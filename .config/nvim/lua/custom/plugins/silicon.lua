dd998835240e9124d5479995400f66e816b552bb
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2024-02-29 22:16:26 +0100

    nvim: configure silicon

--- .config/nvim/lua/custom/plugins/silicon.lua
+++ .config/nvim/lua/custom/plugins/silicon.lua
@@ -1,17 +1,20 @@
 return {
-  "michaelrommel/nvim-silicon",
-  lazy = true,
-  cmd = "Silicon",
+  'michaelrommel/nvim-silicon',
+  lazy = false,
+  cmd = 'Silicon',
   config = function()
-    require("silicon").setup({
-      font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+    require('silicon').setup {
+      font = 'JetBrainsMono Nerd Font=34',
+      background = '#fff0',
+      tab_width = 2,
+      pad_horiz = 50,
+      pad_vert = 40,
       to_clipboard = true,
       no_line_number = true,
-      output = "~/Pictures",
       window_title = function()
-        return vim.fn.fnamemodify(vim.api.nvim_get_current_buf(), ":t")
-      end
-    })
-    vim.keymap.set("v", "<leader>si", ":Silicon<CR>", { desc = "Silicon" })
-  end
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ':t')
+      end,
+    }
+    vim.keymap.set('v', '<leader>si', ':Silicon<CR>', { desc = 'Silicon' })
+  end,
 }
