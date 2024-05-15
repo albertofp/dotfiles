80ae30628db35c52be7b1fa4f76779479a1493fd
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2024-05-16 00:11:37 +0200

    nvim: enable icons in oil

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -6,6 +6,10 @@ return {
     require('oil').setup {
       default_file_explorer = false,
       experimental_watch_for_changes = true,
+      columns = { 'icon' },
+      view_options = {
+        show_hidden = true,
+      },
     }
     vim.keymap.set('n', '_', ':Oil<CR>', { desc = 'Open parent directory', silent = true })
   end,
