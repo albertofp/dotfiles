29129d2bb9fcb975fec4dde8b35bb3dc5fde30f8
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2024-06-09 20:57:50 +0200

    nvim: always_show_bufferline false

--- .config/nvim/lua/custom/plugins/bufferline.lua
+++ .config/nvim/lua/custom/plugins/bufferline.lua
@@ -1,7 +1,11 @@
 return {
   'akinsho/bufferline.nvim',
   config = function()
-    require('bufferline').setup()
+    require('bufferline').setup {
+      options = {
+        always_show_bufferline = false,
+      }
+    }
   end,
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
