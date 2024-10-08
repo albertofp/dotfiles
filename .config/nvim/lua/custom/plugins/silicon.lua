10ba7fda80f3425862ba43091776935afa7dbe6e
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2024-10-08 10:13:41 +0200

    nvim: update silicon config syntax

--- .config/nvim/lua/custom/plugins/silicon.lua
+++ .config/nvim/lua/custom/plugins/silicon.lua
@@ -3,13 +3,16 @@ return {
   lazy = false,
   cmd = 'Silicon',
   config = function()
-    require('silicon').setup {
+    require('nvim-silicon').setup {
       font = 'JetBrainsMono Nerd Font=34',
       background = '#fff0',
       tab_width = 2,
       pad_horiz = 50,
       pad_vert = 40,
       to_clipboard = true,
+      output = function()
+        return "./" .. os.date("!%Y-%m-%dT%H-%M-%SZ") .. "_code.png"
+      end,
       no_line_number = true,
       window_title = function()
         return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ':t')
