755c6f2c384648102e11ad6b1115e6d89a21abb0
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-04-22 09:32:19 +0200

    gemini 2.5 pro preview

--- .config/nvim/lua/custom/plugins/avante.lua
+++ .config/nvim/lua/custom/plugins/avante.lua
@@ -8,7 +8,7 @@ return {
     provider = "gemini",
     gemini = {
       endpoint = "https://generativelanguage.googleapis.com/v1beta/models/",
-      model = "gemini-2.0-flash",
+      model = "gemini-2.5-pro-preview-03-25",
       timeout = 30000,
       temperature = 0,
       -- max_tokens = 4096,
